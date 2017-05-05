6de4f59b796aa3720e761947da5a9891d47cd244
Asmaefahmy <a.fahmy3@umiami.edu>
2017-05-05 13:55:43 -0400

    WEBSITE

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    $("#contact").click(function(){
+    $("#hidden-contact").fadeIn();
+    });
+
+	$("#hidden-box1").hover(function(){
+        $("#hidden-box1").fadeTo(200,0.8);
+			}, function(){
+        $("#hidden-box1").fadeTo(200,0);
+	});
+    
+	$("#hidden-box2").hover(function(){
+        $("#hidden-box2").fadeTo(200,0.8);
+			}, function(){
+        $("#hidden-box2").fadeTo(200,0);
+	});
+
+	$("#hidden-box3").hover(function(){
+        $("#hidden-box3").fadeTo(200,0.8);
+			}, function(){
+        $("#hidden-box3").fadeTo(200,0);
+	});
+
+	$("#hidden-box4").hover(function(){
+        $("#hidden-box4").fadeTo(200,0.8);
+			}, function(){
+        $("#hidden-box4").fadeTo(200,0);
+	});
+    
+	$("#hidden-box5").hover(function(){
+        $("#hidden-box5").fadeTo(200,0.8);
+			}, function(){
+        $("#hidden-box5").fadeTo(200,0);
+	});
+
+	$("#hidden-box6").hover(function(){
+        $("#hidden-box6").fadeTo(200,0.8);
+			}, function(){
+        $("#hidden-box6").fadeTo(200,0);
+	});
+
+	$("#hidden-box7").hover(function(){
+        $("#hidden-box7").fadeTo(200,0.8);
+			}, function(){
+        $("#hidden-box7").fadeTo(200,0);
+	});
+    
+	$("#hidden-box8").hover(function(){
+        $("#hidden-box8").fadeTo(200,0.8);
+			}, function(){
+        $("#hidden-box8").fadeTo(200,0);
+	});
+
+	$("#hidden-box9").hover(function(){
+        $("#hidden-box9").fadeTo(200,0.8);
+			}, function(){
+        $("#hidden-box9").fadeTo(200,0);
+	});
+
+});
